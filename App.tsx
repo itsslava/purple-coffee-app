56cfccccf7970a8301ec07715ff322f7899921ea
itsslava <77506882+itsslava@users.noreply.github.com>
2024-07-24 20:03:21 +0300

    layout: created base layout for main page

--- App.tsx
+++ App.tsx
@@ -1,11 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, Text, View, StyleSheet } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient' 
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <LinearGradient
+        colors={['rgba(0,0,0,0)', 'rgba(0,0,0,0.9)', '#000']}
+        locations={[0, 0.2, 0.3]}
+        style={styles.gradient}
+      >
+        <View style={styles.content}>
+          <Text style={styles.title}>Один из самых вкусных кофе в городе!</Text>
+          <Text style={styles.subtitle}>
+            Свежие зерна, настоящая арабика и бережная обжарка
+          </Text>
+          <Button title="Начать" />
+        </View>
+      </LinearGradient>
     </View>
   );
 }
@@ -13,8 +24,30 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    justifyContent: 'flex-end',
+  },
+  content: {
+    marginTop: 52,
     alignItems: 'center',
-    justifyContent: 'center',
+    paddingHorizontal: 30,
+    // marginBottom: 43,
+  },
+  title: {
+    fontSize: 34,
+    fontWeight: '600',
+    color: '#fff',
+    textAlign: 'center',
+    paddingBottom: 8,
+  },
+  subtitle: {
+    fontSize: 14,
+    textAlign: 'center',
+    paddingBottom: 24,
+    color: '#a9a9a9',
+  },
+  gradient: {
+    height: 362,
+    width: '100%',
   },
 });
+
