99b39f85a271f57fe694e1624e21a32176d2b929
itsslava <77506882+itsslava@users.noreply.github.com>
2024-07-25 13:48:01 +0300

    layout: finished layout for main page
    also added custom button component, colors for design system

--- App.tsx
+++ App.tsx
@@ -1,36 +1,52 @@
-import { Button, Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, ImageBackground } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient' 
+import Button from './shared/button/button';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <LinearGradient
-        colors={['rgba(0,0,0,0)', 'rgba(0,0,0,0.9)', '#000']}
-        locations={[0, 0.2, 0.3]}
-        style={styles.gradient}
+      <ImageBackground
+      source={require('./assets/images/coffee-img.png')}
+      resizeMode='cover'
+      style={styles.image}
       >
-        <View style={styles.content}>
-          <Text style={styles.title}>Один из самых вкусных кофе в городе!</Text>
-          <Text style={styles.subtitle}>
-            Свежие зерна, настоящая арабика и бережная обжарка
-          </Text>
-          <Button title="Начать" />
-        </View>
+      <LinearGradient
+          colors={['rgba(0,0,0,0)', 'rgba(0,0,0,0.9)', '#000']}
+          locations={[0, 0.2, 0.3]}
+          style={styles.gradient}
+      >
+      <View style={styles.content}>
+        <Text style={styles.title}>Один из самых вкусных кофе в городе!</Text>
+        <Text style={styles.subtitle}>
+          Свежие зерна, настоящая арабика и бережная обжарка
+        </Text>
+        <Button text="Начать" />
+      </View>
       </LinearGradient>
+      </ImageBackground>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+   flex: 1,
+  },
+  image: {
     flex: 1,
+    height: '70%',
     justifyContent: 'flex-end',
   },
+  gradient: {
+    height: 360,
+    width: '100%',
+    // justifyContent: 'flex-end',
+  },
   content: {
-    marginTop: 52,
-    alignItems: 'center',
+    justifyContent: 'center',
     paddingHorizontal: 30,
     // marginBottom: 43,
+    marginTop: 52,
   },
   title: {
     fontSize: 34,
@@ -45,9 +61,4 @@ const styles = StyleSheet.create({
     paddingBottom: 24,
     color: '#a9a9a9',
   },
-  gradient: {
-    height: 362,
-    width: '100%',
-  },
 });
-
