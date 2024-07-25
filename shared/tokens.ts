7ce5aa1b4de12d0c64a0a50611accb4d23a17bc9
itsslava <77506882+itsslava@users.noreply.github.com>
2024-07-25 15:49:27 +0300

    add: color, font and radius sizes for design system

--- shared/tokens.ts
+++ shared/tokens.ts
@@ -3,4 +3,19 @@ export const Colors = {
   white: '#fff',
   grey: '#a9a9a9',
   primary: '#c67c4e',
+  primaryHover: '#a76237'
+};
+
+export const Radius = {
+  r16: 16,
+  r12: 12,
+  r10: 10
+}
+
+export const Font = {
+  f34: 34,
+  f18: 18,
+  f16: 16,
+  f14: 14,
+  f12: 12
 };
