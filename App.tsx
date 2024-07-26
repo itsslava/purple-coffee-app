321c0cbc2391a4fcba10310b8c2304a5ea409aa4
itsslava <77506882+itsslava@users.noreply.github.com>
2024-07-26 11:27:21 +0300

    lint: added eslint config

--- App.tsx
+++ App.tsx
@@ -1,101 +1,99 @@
 import { Text, View, StyleSheet, ImageBackground, Animated } from 'react-native';
-import { LinearGradient } from 'expo-linear-gradient' 
+import { LinearGradient } from 'expo-linear-gradient';
 import Button from './shared/button/button';
 import { Colors, Font } from './shared/tokens';
 import { useEffect } from 'react';
 
 export default function App() {
-  const animatedTransform = new Animated.ValueXY({
-    x: 0,
-    y: -100
-  })
-  const animatedOpacity = new Animated.Value(0)
+	const animatedTransform = new Animated.ValueXY({
+		x: 0,
+		y: -100,
+	});
+	const animatedOpacity = new Animated.Value(0);
 
-  useEffect(() => {
-    Animated.parallel([
-      Animated.timing(animatedOpacity, {
-        toValue: 100,
-        duration: 10000,
-        useNativeDriver: true,
-      }),
-      Animated.timing(animatedTransform, {
-        toValue: {
-          x: 0,
-          y: 0,
-        },
-        duration: 1000,
-        useNativeDriver: true,
-      }),
-    ]).start();
-  }, [])
+	useEffect(() => {
+		Animated.parallel([
+			Animated.timing(animatedOpacity, {
+				toValue: 100,
+				duration: 10000,
+				useNativeDriver: true,
+			}),
+			Animated.timing(animatedTransform, {
+				toValue: {
+					x: 0,
+					y: 0,
+				},
+				duration: 1000,
+				useNativeDriver: true,
+			}),
+		]).start();
+	}, []);
 
-  return (
-    <View style={styles.container}>
-      <ImageBackground
-        source={require('./assets/images/coffee-img.png')}
-        resizeMode="cover"
-        style={styles.image}
-      >
-        <LinearGradient
-          colors={['rgba(0,0,0,0)', 'rgba(0,0,0,0.9)', '#000']}
-          locations={[0, 0.2, 0.3]}
-          style={styles.gradient}
-        >
-          <View style={styles.content}>
-            <Animated.Text
-              style={{
-                ...styles.title,
-                transform: [
-                  { translateX: animatedTransform.x },
-                  { translateY: animatedTransform.y },
-                ],
-                opacity: animatedOpacity
-              }}
-            >
-              Один из самых вкусных кофе в городе!
-            </Animated.Text>
-            <Text style={styles.subtitle}>
-              Свежие зерна, настоящая арабика и бережная обжарка
-            </Text>
-            <Button text="Начать" />
-          </View>
-        </LinearGradient>
-      </ImageBackground>
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<ImageBackground
+				source={require('./assets/images/coffee-img.png')}
+				resizeMode="cover"
+				style={styles.image}
+			>
+				<LinearGradient
+					colors={['rgba(0,0,0,0)', 'rgba(0,0,0,0.9)', '#000']}
+					locations={[0, 0.2, 0.3]}
+					style={styles.gradient}
+				>
+					<View style={styles.content}>
+						<Animated.Text
+							style={{
+								...styles.title,
+								transform: [
+									{ translateX: animatedTransform.x },
+									{ translateY: animatedTransform.y },
+								],
+								opacity: animatedOpacity,
+							}}
+						>
+							Один из самых вкусных кофе в городе!
+						</Animated.Text>
+						<Text style={styles.subtitle}>Свежие зерна, настоящая арабика и бережная обжарка</Text>
+						<Button text="Начать" />
+					</View>
+				</LinearGradient>
+			</ImageBackground>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  image: {
-    flex: 1,
-    height: '70%',
-    justifyContent: 'flex-end',
-  },
-  gradient: {
-    height: 360,
-    width: '100%',
-    // justifyContent: 'flex-end',
-  },
-  content: {
-    justifyContent: 'center',
-    paddingHorizontal: 30,
-    // marginBottom: 43,
-    marginTop: 52,
-  },
-  title: {
-    fontSize: Font.f34,
-    fontWeight: '600',
-    color: Colors.white,
-    textAlign: 'center',
-    paddingBottom: 8,
-  },
-  subtitle: {
-    fontSize: Font.f14,
-    textAlign: 'center',
-    paddingBottom: 24,
-    color: Colors.grey,
-  },
+	container: {
+		flex: 1,
+	},
+	image: {
+		flex: 1,
+		height: '70%',
+		justifyContent: 'flex-end',
+	},
+	gradient: {
+		height: 360,
+		width: '100%',
+		// justifyContent: 'flex-end',
+	},
+	content: {
+		justifyContent: 'center',
+		paddingHorizontal: 30,
+		marginTop: 52,
+		// marginBottom: 43,
+	},
+	title: {
+		fontSize: Font.f34,
+		fontWeight: '600',
+		color: Colors.white,
+		textAlign: 'center',
+		paddingBottom: 8,
+	},
+	subtitle: {
+		fontSize: Font.f14,
+		textAlign: 'center',
+		paddingBottom: 24,
+		color: Colors.grey,
+	},
 });
--- shared/tokens.ts
+++ shared/tokens.ts
@@ -1,21 +1,21 @@
 export const Colors = {
-  black: '#000',
-  white: '#fff',
-  grey: '#a9a9a9',
-  primary: '#c67c4e',
-  primaryHover: '#a76237'
+	black: '#000',
+	white: '#fff',
+	grey: '#a9a9a9',
+	primary: '#c67c4e',
+	primaryHover: '#a76237',
 };
 
 export const Radius = {
-  r16: 16,
-  r12: 12,
-  r10: 10
-}
+	r16: 16,
+	r12: 12,
+	r10: 10,
+};
 
 export const Font = {
-  f34: 34,
-  f18: 18,
-  f16: 16,
-  f14: 14,
-  f12: 12
+	f34: 34,
+	f18: 18,
+	f16: 16,
+	f14: 14,
+	f12: 12,
 };
