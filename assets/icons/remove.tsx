c4b4a5a1570160ca248b9f5a59c23a44be30b9cd
itsslava <77506882+itsslava@users.noreply.github.com>
2024-08-13 22:42:57 +0300

    feat: add two new SVG icons

---
+++ assets/icons/remove.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import Svg, { Path } from 'react-native-svg';
+const RemoveIcon = () => (
+	<Svg width={16} height={16} fill="none">
+		<Path stroke="#fff" strokeLinecap="round" strokeLinejoin="round" strokeWidth={1.5} d="M4 8h8" />
+	</Svg>
+);
+export default RemoveIcon;
