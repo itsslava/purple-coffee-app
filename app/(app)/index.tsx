28b07376bde361db3aa9326f42788d821c9402e5
itsslava <77506882+itsslava@users.noreply.github.com>
2024-08-19 17:17:22 +0300

    feat: integrate CoffeeFilter component and implement filtering logic in catalog

--- app/(app)/index.tsx
+++ app/(app)/index.tsx
@@ -1,24 +1,37 @@
 import { useAtomValue, useSetAtom } from 'jotai';
 import { ActivityIndicator, StyleSheet, View } from 'react-native';
-import { coffeeAtom, loadCoffeeAtom } from '../../entities/coffee/model/coffee.state';
+import {
+	coffeeAtom,
+	loadCoffeeAtom,
+	setFilterAtom,
+} from '../../entities/coffee/model/coffee.state';
 import { useEffect } from 'react';
 import { CoffeeCard } from '../../widget/coffee/ui/coffee-card/coffee-card';
 import { Colors } from '../../shared/tokens';
+import { CoffeeFilter } from '../../widget/coffee/ui/coffee-filter/coffee-filter';
+import { DrinkFilter } from '../../entities/coffee/model/coffee.model';
 
 export default function Home() {
-	const { coffee, isLoading } = useAtomValue(coffeeAtom);
+	const { isLoading, coffee, filter } = useAtomValue(coffeeAtom);
 	const loadCoffee = useSetAtom(loadCoffeeAtom);
+	const setFilter = useSetAtom(setFilterAtom);
 
 	useEffect(() => {
 		loadCoffee();
 	}, []);
 
+	const handleFilterChange = (filter: DrinkFilter) => {
+		setFilter(filter);
+		loadCoffee(filter);
+	};
+
 	return (
 		<View style={styles.container}>
+			<CoffeeFilter onFilterChange={handleFilterChange} currentFilter={filter ?? 'all'} />
 			{isLoading && (
 				<ActivityIndicator size="large" color={Colors.primary} style={styles.indicator} />
 			)}
-			{coffee.length > 0 && (
+			{!isLoading && coffee.length > 0 && (
 				<View style={styles.coffeeList}>
 					{coffee.map((c) => (
 						<CoffeeCard key={c.id} coffee={c} onPress={() => console.log('Pressed')} />
